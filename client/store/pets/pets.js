4f274e6ceb2033b6dc41d16015c7e0178dcf8d52
Priscilla <priscillakim58@gmail.com>
2022-02-07 21:26:24 -0500

    user and pets load

--- client/store/pets/pets.js
+++ client/store/pets/pets.js
@@ -1,29 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
-const GET_PETS = 'GET_PETS';
+const GET_PETS = "GET_PETS";
 
 //action object
 const _loadPets = (pets) => {
   return {
     type: GET_PETS,
-    payload: pets
+    payload: pets,
   };
 };
 
 //thunks
 export const loadPets = () => {
   return async (dispatch) => {
-    const pets = (await axios.get('/api/pets')).data;
+    const pets = (await axios.get("/api/pets")).data;
     dispatch(_loadPets(pets));
   };
 };
 
 //reducer
-export default function petsReducer (state=[], action) {
-  switch(action.type) {
+export default function petsReducer(state = [], action) {
+  switch (action.type) {
     case GET_PETS:
       return action.payload;
     default:
       return state;
-  };
-};
+  }
+}
