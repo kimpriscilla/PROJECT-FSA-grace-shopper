c60b8c358f5cdbd378ffa051cb8e645b318852eb
Priscilla <priscillakim58@gmail.com>
2022-02-09 19:05:36 -0500

    single user edit

--- client/components/SingleUser.js
+++ client/components/SingleUser.js
@@ -1,22 +1,31 @@
 //!CURRENTLY NOT WORKING
 
-/*
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import loadUsers from "../store/users/users";
 
 class SingleUser extends Component {
   constructor() {
     super();
   }
-  async componentDidMount() {
-    this.props.loadUsers();
-  }
   render() {
-    console.log("----->", this.props.user ? this.props.user.email : "idk");
+    console.log("??????", this.props.user?.orders[0]);
     return (
       <>
         <h1>SINGLE USER PAGE// ADMIN VIEW ONLY</h1>
+        <ul id="userCard"></ul>
+
+        <ul id="individualCards">
+          <li>
+            <img src={this.props.user?.imageUrl}></img>
+          </li>
+          <li> EMAIL: {this.props.user?.email}</li>
+          <li>
+            ORDERS:
+            {/* {this.props.user?.orders
+              ? this.props.user?.orders[0]
+              : "NO ORDERS YET"}{" "} */}
+          </li>
+        </ul>
       </>
     );
   }
@@ -26,20 +35,11 @@ const mapState = (state, ownProps) => {
   const user = state.users.find(
     (user) => user.id === ownProps.match.params.id * 1
   );
-  console.log("SINGLE USER--->", user);
+
   return {
     user,
     ownProps,
   };
 };
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadUsers: () => {
-      dispatch(loadUsers());
-    },
-  };
-};
-
-export default connect(mapState, mapDispatch)(SingleUser);
-*/
+export default connect(mapState, null)(SingleUser);
