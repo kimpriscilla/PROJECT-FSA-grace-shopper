7725225ff4fc2d3dce94b9d2a55b57f57df9a74c
Priscilla <priscillakim58@gmail.com>
2022-02-10 21:22:47 -0500

    allDogs

--- client/components/allDogs.js
+++ client/components/allDogs.js
@@ -10,7 +10,6 @@ const Dogs = ({ loading, pets, addCart, pet }) => {
   if (loading) {
     return <h2>Loading...</h2>;
   }
-
   return (
     <div id="rightAllDogs">
       <ul id="dogCards">
@@ -26,38 +25,6 @@ const Dogs = ({ loading, pets, addCart, pet }) => {
               <li>Born on: {dog.dateOfBirth}</li>
               <li>
                 <Link to={`/dogs/:${dog.id}`}> More Details </Link>
-
-
-  render() {
-    return (
-      <div>
-        <h3>Welcome, allDogs </h3>
-        <div id="leftAllDogs"></div>
-        <div id="rightAllDogs">
-          <ul id="dogCards">
-            {this.props.pets.map((dog) => (
-              <li key={dog.id}>
-                <ul id="individualCards">
-                  <li>
-                    <img src={dog.imageUrl} />
-                  </li>
-                  <li>Name: {dog.name} </li>
-                  <li>Gender: {dog.gender} </li>
-                  {/* <li>Breed: {dog.breed.name} </li> ADD ONCE DATA SEEDS DOG BREED*/}
-                  <li>Born on: {dog.dateOfBirth}</li>
-                  <li>
-                    <Link to={`/dogs/:${dog.id}`}> More Details </Link>
-                  </li>
-                  <li>
-                    <button
-                      className="button-37"
-                      role="button"
-                      onClick={() => this.props.addCart(tempUserId, dog.id)}
-                    >
-                      Add to Cart
-                    </button>
-                  </li>
-                </ul>
               </li>
               <li>
                 <button
