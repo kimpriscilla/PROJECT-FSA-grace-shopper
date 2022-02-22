2aa0c3deb3cef3acb37162c9515554a7202dc1c3
jenniferlu32 <jenniferlu32@gmail.com>
2022-02-21 19:14:37 -0500

    edit user fix

--- client/components/Analytics.js
+++ client/components/Analytics.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { getBreedSales } from '../store/analytics/analytics';
 import { useSelector } from "react-redux";
+import { Bar } from 'react-chartjs-2';
 
 export default function Analytics() {
   const sales = useSelector((state) => state.analytics);
-  console.log(sales)
+  const UserData = [
+    {
+      id: 1,
+      year: 2016,
+      userGain: 80000,
+      userLost: 823,
+    },
+    {
+      id: 2,
+      year: 2017,
+      userGain: 45677,
+      userLost: 345,
+    },
+    {
+      id: 3,
+      year: 2018,
+      userGain: 78888,
+      userLost: 555,
+    },
+    {
+      id: 4,
+      year: 2019,
+      userGain: 90000,
+      userLost: 4555,
+    },
+    {
+      id: 5,
+      year: 2020,
+      userGain: 4300,
+      userLost: 234,
+    },
+  ];
+
   return (
     <div>
-      Analytics
+      <Bar data={UserData} />
     </div>
   );
 };
--- server/api/users.js
+++ server/api/users.js
@@ -27,7 +27,7 @@ router.post("/", async (req, res, next) => {
         email: req.body.email
       }
     });
-    if (findUser) {
+    if (findUser===[]) {
       res.json('User already exists')
     } else {
       const newUser = await User.create(req.body);
